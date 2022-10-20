05754096a18f18649fc08ab6b98cbdea3d690639
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-10-20 05:14:39 +0000

    [Start]応用課題３

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
+  get 'favorites/create'
+  get 'favorites/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   
   root to: "homes#top"
   get "home/about"=>"homes#about"
-
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
+  resources :books, only: [:new, :create, :index, :show, :destroy] do
+  resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
