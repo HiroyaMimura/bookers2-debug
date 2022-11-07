69727d5ddfc2195ee5abe93044c285b85dfe8ff9
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-11-07 04:47:54 +0000

    [Finish]応用課題８c

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,15 @@ Rails.application.configure do
   config.hosts << "e5b6a5dfe09d4200af6730eb136487b2.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            '<YOUR EMAIL ADDRESS>',
+    password:             '<YOUR EMAIL PASSWORD>',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
